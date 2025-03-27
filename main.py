eaa2566ad500497a4e242249cb43e6e258abb0da
Patrick Mateus <noblemateus@gmail.com>
2025-03-27 22:35:40 +0200

    changed host

--- main.py
+++ main.py
@@ -292,4 +292,4 @@ app.include_router(councilor.router)
 app.include_router(auth.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0")
+    uvicorn.run("main:app", reload=True)
